996cb56739647852e367a7c78348fe7794775a5b
pakeha-by <pakeha07@gmail.com>
2026-03-07 23:03:10 +1300

    Support unsalted stream URLs

--- addons/plugin.video.hdrezka.tv/voidboost.py
+++ addons/plugin.video.hdrezka.tv/voidboost.py
@@ -1,5 +1,6 @@
 import re
 import base64
+import helpers
 from operator import itemgetter
 
 BK_SEP = '//_//'
@@ -13,10 +14,19 @@ BK_BLOCKS = [
 
 
 def parse_streams(salted):
-    salted = salted.replace('\\', '')
-    for bk in BK_BLOCKS:
-        salted = salted.replace(BK_SEP + bk, '')
-    decoded_streams = base64.b64decode(salted[2:]).decode('utf-8')
+    origin = salted
+    try:
+        salted = salted.replace('\\', '')
+        for bk in BK_BLOCKS:
+            salted = salted.replace(BK_SEP + bk, '')
+
+        helpers.log('Stripped salted string')
+        helpers.log(salted)
+        decoded_streams = base64.b64decode(salted[2:]).decode('utf-8')
+
+    except Exception as e:
+        decoded_streams = origin
+
     parsed_streams = []
     for name, quality, url in re.findall(r'\[((\d+)[^]]+)].+?(http.+?mp4)', decoded_streams):
         parsed_streams.append((
